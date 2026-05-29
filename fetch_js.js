c6c9cf48c85448be6a2460ff81b3a77d1b11f565
Ngọc Hân Trần <tranngochan29052004@gmail.com>
2026-05-29 14:29:25 +0700

    delete the old file

--- fetch_js.js
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-https.get('https://aptiskey.com/reading_question2.html', (res) => {
-  let data = '';
-  res.on('data', chunk => data += chunk);
-  res.on('end', () => {
-    const scripts = data.match(/<script[^>]+src="([^">]+)"/g);
-    console.log(scripts);
-  });
-});
--- replace.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('aptis-listening-fe/src/app/page.js', 'utf8');
-let newContent = content.replace(/\/\/ ── Auto Answer Logic(.*?)\];/s, '');
-fs.writeFileSync('aptis-listening-fe/src/app/page.js', newContent);
